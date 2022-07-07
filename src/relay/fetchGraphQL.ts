2941426981df8e084562fe7d074ec4ee0ae04112
Leandro Alencar <zzleandrobritozz@gmail.com>
2022-07-07 14:19:25 -0300

    deploy

--- src/relay/fetchGraphQL.ts
+++ src/relay/fetchGraphQL.ts
@@ -1,17 +1,20 @@
 import { Variables } from 'relay-runtime/lib/util/RelayRuntimeTypes'
 
 export const fetchGraphQL = async (query: string, variables: Variables) => {
-  const response = await fetch('http://localhost:9000/graphql', {
-    method: 'POST',
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Content-type': 'application/json'
-    },
-    body: JSON.stringify({
-      query,
-      variables
-    })
-  })
+  const response = await fetch(
+    'https://graphql-koa-app.herokuapp.com/graphql',
+    {
+      method: 'POST',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        query,
+        variables
+      })
+    }
+  )
 
   const data = await response.json()
   return data
